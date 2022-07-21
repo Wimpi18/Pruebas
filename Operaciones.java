b10a69cdf4e6b8c1df12dbf829e20553ca70749a
Winsor <202108302@est.umss.edu>
2022-07-21 11:40:03 -0400

    Se añadio la funcion multiplicar

--- Operaciones.java
+++ Operaciones.java
@@ -1,10 +1,15 @@
 public class Operaciones {
     public static void main(String args[]) {
         int a = 2, b = 4;
-        System.out.println("La suma es: " + sumar(a, b));
+        System.out.println("La suma de " + a + " y " + b + " es: " + sumar(a, b));
+        System.out.println("La multiplicacion de " + a + " y " + b + " es: " + multiplicar(a, b));
     }
 
-    public static int sumar(int a, int b){
-        return  a + b;
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a - b;
     }
 }
