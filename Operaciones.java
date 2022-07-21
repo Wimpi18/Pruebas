9aa68d56e3455147ade3d405ae68355a80e570db
Winsor <202108302@est.umss.edu>
2022-07-21 11:35:10 -0400

    Agrego clase operaciones y elimino el resto

---
+++ Operaciones.java
@@ -0,0 +1,10 @@
+public class Operaciones {
+    public static void main(String args[]) {
+        int a = 2, b = 4;
+        System.out.println("La suma es: " + sumar(a, b));
+    }
+
+    public static int sumar(int a, int b){
+        return  a + b;
+    }
+}
