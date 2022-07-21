325766cf0da776db2c9e928ef29b84b817eace83
Winsor <202108302@est.umss.edu>
2022-07-21 12:27:49 -0400

    Funcion restar agregada

--- Operaciones.java
+++ Operaciones.java
@@ -3,6 +3,7 @@ public class Operaciones {
         int a = 2, b = 4;
         System.out.println("La suma de " + a + " y " + b + " es: " + sumar(a, b));
         System.out.println("La multiplicacion de " + a + " y " + b + " es: " + multiplicar(a, b));
+        System.out.println("La resta de " + a + " y " + b + " es: " + restar(a, b));
     }
 
     public static int sumar(int a, int b) {
@@ -10,6 +11,10 @@ public class Operaciones {
     }
 
     public static int multiplicar(int a, int b) {
+        return a - b;
+    }
+
+    public static int restar(int a, int b) {
         return a - b;
     }
 }
